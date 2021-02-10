baa287efde1ec27bb921a2ae468c063fedcaf4fe
zkzhe <1zkzhe1146103@gmail.com>
2021-02-11 03:20:03 +0800

    Ecommerce Product Delete & Active & Inactive

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -11,6 +11,13 @@ class ProductController extends Controller
 {
     public function index()
     {
+        $product = DB::table('products')
+            ->join('categories', 'products.category_id', 'categories.id')
+            ->join('brands', 'products.brand_id', 'brands.id')
+            ->select('products.*', 'categories.category_name', 'brands.brand_name')
+            ->get();
+        // return response()->json($product);
+        return view('admin.product.index', compact('product'));
     }
 
     public function create()
@@ -55,15 +62,15 @@ class ProductController extends Controller
         if ($image_one && $image_two && $image_three) {
             $image_one_name = hexdec(uniqid()) . '.' . $image_one->getClientOriginalExtension();
             Image::make($image_one)->resize(300, 300)->save('media/product/' . $image_one_name);
-            $data['image_one'] = 'public/media/product' . $image_one_name;
+            $data['image_one'] = 'media/product/' . $image_one_name;
 
             $image_two_name = hexdec(uniqid()) . '.' . $image_two->getClientOriginalExtension();
             Image::make($image_two)->resize(300, 300)->save('media/product/' . $image_two_name);
-            $data['image_one'] = 'public/media/product' . $image_two_name;
+            $data['image_two'] = 'media/product/' . $image_two_name;
 
             $image_three_name = hexdec(uniqid()) . '.' . $image_three->getClientOriginalExtension();
             Image::make($image_three)->resize(300, 300)->save('media/product/' . $image_three_name);
-            $data['image_one'] = 'public/media/product' . $image_three_name;
+            $data['image_three'] = 'media/product/' . $image_three_name;
 
             $product = DB::table('products')->insert($data);
             $notification = array(
@@ -72,5 +79,75 @@ class ProductController extends Controller
             );
             return redirect()->back()->with($notification);
         }
+    }
+
+    public function Inactive($id)
+    {
+        DB::table('products')->where('id', $id)->update(['status' => 0]);
+        $notification = array(
+            'message' => 'Product Inactive Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+
+    public function Active($id)
+    {
+        DB::table('products')->where('id', $id)->update(['status' => 1]);
+        $notification = array(
+            'message' => 'Product Active Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+
+    public function EditProduct($id)
+    {
+        $product = DB::table('products')->where('id', $id)->first();
+        $image_one = $product->image_one;
+        $image_two = $product->image_two;
+        $image_three = $product->image_three;
+
+        unlink($image_one);
+        unlink($image_two);
+        unlink($image_three);
+
+        DB::table('products')->where('id', $id)->delete();
+
+        $notification = array(
+            'message' => 'Product Deleted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+
+    public function DeleteProduct($id)
+    {
+        $product = DB::table('products')->where('id', $id)->first();
+        $image_one = $product->image_one;
+        $image_two = $product->image_two;
+        $image_three = $product->image_three;
+
+        @unlink($image_one);
+        @unlink($image_two);
+        @unlink($image_three);
+
+        DB::table('products')->where('id', $id)->delete();
+
+        $notification = array(
+            'message' => 'Product Deleted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+
+    public function ShowProduct($id)
+    {
+        DB::table('products')->where('id', $id)->update(['status' => 1]);
+        $notification = array(
+            'message' => 'Product Active Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -60,36 +60,41 @@ Route::post('/admin/profile/store', [MainAdminController::class, 'AdminProfileSt
 Route::get('/admin/change/password', [MainAdminController::class, 'AdminChangePassword'])->name('admin.change.password');
 Route::post('/admin/password/update', [MainAdminController::class, 'AdminChangePasswordUpdate'])->name('admin.password.update');
 Route::post('/admin/password/update', [MainAdminController::class, 'AdminChangePasswordUpdate'])->name('admin.password.update');
-
+//categories
 Route::get('/admin/categories', [CategoryController::class, 'AdminCategory'])->name('admin.categories');
 Route::post('/admin/store/category', [CategoryController::class, 'AdminStoreCategory'])->name('admin.store.category');
 Route::get('/admin/edit/category/{id}', [CategoryController::class, 'AdminEditCategory']);
 Route::post('/admin/update/category/{id}', [CategoryController::class, 'AdminUpdateCategory']);
 Route::get('/admin/delete/category/{id}', [CategoryController::class, 'AdminDeleteCategory']);
-
+//brands
 Route::get('/admin/brands', [BrandController::class, 'Adminbrand'])->name('admin.brands');
 Route::post('/admin/store/brand', [BrandController::class, 'AdminStoreBrand'])->name('admin.store.brand');
 Route::get('/admin/edit/brand/{id}', [BrandController::class, 'AdminEditBrand']);
 Route::post('/admin/update/brand/{id}', [BrandController::class, 'AdminUpdateBrand']);
 Route::get('/admin/delete/brand/{id}', [BrandController::class, 'AdminDeleteBrand']);
-
+//subcategories
 Route::get('/admin/subcategories', [SubCategoryController::class, 'AdminSubcategories'])->name('admin.sub.categories');
 Route::post('/admin/store/subcategory', [SubCategoryController::class, 'AdminStoreSubcategory'])->name('admin.store.subcategories');
 Route::get('/admin/edit/subcategory/{id}', [SubCategoryController::class, 'AdminEditSubcategory']);
 Route::post('/admin/update/subcategory/{id}', [SubCategoryController::class, 'AdminUpdateSubcategory']);
 Route::get('/admin/delete/subcategory/{id}', [SubCategoryController::class, 'AdminDeleteSubcategory']);
-
+//coupon
 Route::get('/admin/coupon', [CouponController::class, 'AdminCoupon'])->name('admin.coupon');
 Route::post('/admin/store/coupon', [CouponController::class, 'AdminStoreCoupon'])->name('admin.store.coupon');
 Route::get('/admin/edit/coupon/{id}', [CouponController::class, 'AdminEditCoupon']);
 Route::post('/admin/update/coupon/{id}', [CouponController::class, 'AdminUpdateCoupon']);
 Route::get('/admin/delete/coupon/{id}', [CouponController::class, 'AdminDeleteCoupon']);
-
+//newslater
 Route::get('/admin/newslater', [CouponController::class, 'AdminNewslater'])->name('admin.newslater');
 Route::post('/store/newslater', [FrontController::class, 'StoreNewslater'])->name('store.newslater');
 Route::get('/delete/newslater/{id}', [FrontController::class, 'DeleteNewslater']);
-
+//product
 Route::get('/admin/product/all', [ProductController::class, 'index'])->name('all.product');
 Route::get('/admin/product/add', [ProductController::class, 'create'])->name('add.product');
 Route::get('/admin/get/subcategory/{subcategory_id}', [ProductController::class, 'GetSubcat']);
 Route::post('/admin/store/product', [ProductController::class, 'store'])->name('store.product');
+Route::get('/admin/edit/product/{id}', [ProductController::class, 'EditProduct']);
+Route::get('/admin/delete/product/{id}', [ProductController::class, 'DeleteProduct']);
+Route::get('/admin/show/product/{id}', [ProductController::class, 'ShowProduct']);
+Route::get('/admin/inactive/product/{id}', [ProductController::class, 'Inactive']);
+Route::get('/admin/active/product/{id}', [ProductController::class, 'Active']);
